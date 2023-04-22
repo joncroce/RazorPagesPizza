ab3cbece8f4e67276822a03d4488c6113db3cf5f
Jon Croce <crocedev@gmail.com>
2023-04-21 23:07:55 -0400

    Fix incorrect nextId declaration

--- Services/PizzaService.cs
+++ Services/PizzaService.cs
@@ -5,7 +5,7 @@ namespace RazorPagesPizza.Services;
 public static class PizzaService
 {
 	static List<Pizza> Pizzas { get; }
-	static int nextId = 3;
+	static int nextId = 5;
 	static PizzaService()
 	{
 		Pizzas = new List<Pizza>
